73ecfd448cd06b17ab845cd2260446f59fb9fcbc
Agnija <ilzena.agnija@gmail.com>
2022-01-10 09:58:49 +0100

    upd

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/debug.php';
     require_once __DIR__ . '/../config/db.php';
