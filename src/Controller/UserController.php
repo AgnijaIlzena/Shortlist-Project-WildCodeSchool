d84f8d826f5bb942670520d07745be7b08a5e68b
Agnija <ilzena.agnija@gmail.com>
2022-01-13 14:24:52 +0100

    progress commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,78 +2,14 @@
 
 namespace App\Controller;
 
-use App\Model\ItemManager;
 use App\Model\UserManager;
 
 class UserController extends AbstractController
 {
-    public function show(int $id): string
-    //Select 1 user by ID
+    public function selectMyUser(): ?string
     {
         $userManager = new UserManager();
-        $user = $userManager->selectOneById($id);
-
-        return $this->twig->render('User/show.html.twig', ['user' => $user]);
+        $user = $userManager->selectCheckedIn();
+        return $this->twig->render('User/LogedInUserLook.html.twig', ['user' => $user]);
     }
-
-
-    public function index(): string
-    {
-        $userManager = new UserManager();
-        $user = $userManager->selectAll('user_name');
-        return $this->twig->render('User/user.html.twig', ['user' => $user]);
-    }
-
-    public function add(): string
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $user = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, insert and redirection
-            $userManager = new UserManager();
-            $id = $userManager->insert($user);
-            header('Location:/user/show?id=' . $id);
-        }
-
-        return $this->twig->render('User/add.html.twig');
-    }
-
-
-
-    public function edit(int $id): string
-    {
-        $userManager = new UserManager();
-        $user = $userManager->selectOneById($id);
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $user = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, update and redirection
-            //  $itemManager->update($item);
-            header('Location: /user/show?id=' . $id);
-        }
-
-        return $this->twig->render('User/edit.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
-
-
-    public function delete()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $id = trim($_POST['id']);
-            $userManager = new UserManager();
-            $userManager->delete((int)$id);
-            header('Location: /user');
-        }
-    }
-
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -6,42 +6,20 @@ class UserManager extends AbstractManager
 {
     public const TABLE = 'user';
 
-    public function insert(array $user): int
+    public function selectCheckedIn(): ?array
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`user_name`,`email`,`passowrd`) VALUES (:user_name, :email, :passowrd)");
-        $statement->bindValue('user_name', $user['user_name'], \PDO::PARAM_STR);
-        $statement->bindValue('email', $user['email'], \PDO::PARAM_STR);
-        $statement->bindValue('passowrd', $user['passowrd'], \PDO::PARAM_STR);
-
-        $statement->execute();
-        return (int)$this->pdo->lastInsertId();
-    }
-
-
-
-
-    public function checkid(string $id): ?array
-    {
+        if(isset($_SESSION['id'])) {
         $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE id = :id");
-        $statement->bindValue('id', $id, \PDO::PARAM_STR);
+        $statement->bindValue('id', $_SESSION['id']);
         $statement->execute();
         $result = $statement->fetch();
-        if ($result) {
-            return $result;
-        } else {
-            return null;
+                    if ($result) {
+                return $result;
+            } else {
+                return null;
         }
-    }
-
-
-
-    public function selectOneById(int $id)
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
+            } else {
+                return null;
+            }
     }
 }
