2569dccc459632bdec61168e167ee75cfe70941c
Agnija <ilzena.agnija@gmail.com>
2022-01-13 14:32:49 +0100

    progress commit

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -9,17 +9,15 @@ class UserManager extends AbstractManager
     public function selectCheckedIn(): ?array
     {
         if(isset($_SESSION['id'])) {
-        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE id = :id");
-        $statement->bindValue('id', $_SESSION['id']);
-        $statement->execute();
-        $result = $statement->fetch();
-                    if ($result) {
+            $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE id = :id");
+            $statement->bindValue('id', $_SESSION['id']);
+            $statement->execute();
+            $result = $statement->fetch();
+            if ($result) {
                 return $result;
             } else {
                 return null;
-        }
-            } else {
-                return null;
             }
+        } else {return null;}
     }
 }
